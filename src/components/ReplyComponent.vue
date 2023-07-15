db4d01121415f73de0cdf27f0f43351f876d7e21
MuchiriAndrew <kariukia225@gmail.com>
2023-07-15 16:13:55 +0300

    update

--- src/components/ReplyComponent.vue
+++ src/components/ReplyComponent.vue
@@ -1,5 +1,5 @@
 <template>
-       <div v-if="showReply" id="comments" class="bg-light rounded-2 mb-1">
+       <div v-for="reply in replyData" :key="reply"  id="comments" class="bg-light rounded-2 mb-1">
             <div id="counter-div" >
             <div id="count" class="rounded-3">
                 <span id="ops"><b>+</b></span>
@@ -13,9 +13,9 @@
 
             <div id="comment-info">
                 <div id="first-info">
-                <img src="../assets/avatars/image-amyrobson.png" alt="amyrobson">
-                <h6 class="m-0"><b>amyrobson</b></h6>
-                <span>1 month ago</span>
+                <img src="./images/avatars/image-amyrobson.png" alt="amyrobson">
+                <h6 class="m-0"><b>{{reply.user.username}}</b></h6>
+                <span>{{reply.createdAt}}</span>
                 </div>
 
                 <div id="second-info">
@@ -26,7 +26,7 @@
             </div>
 
             <div id="comment-text" class="pt-2">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse quidem eum quis, minus, consectetur ab ad sit laudantium unde magnam consequuntur. Soluta nihil, provident maxime debitis quod exercitationem, rem et, reiciendis error suscipit facilis? Minima quidem quos qui vero labore optio maiores, expedita deleniti illo aliquid sunt id doloribus nostrum.</p>
+                <p>{{reply.content}}</p>
             </div>
 
             </div>
@@ -35,8 +35,29 @@
 </template>
 
 <script>
+import commentData from "../data.json"
 export default {
-    props:['showReply',' handleReply']
+    name: 'ReplyComponent',
+  data() {
+    return {
+        myData : commentData.comments,
+        replyData: [],
+    }
+  },
+
+
+    
+mounted() {
+    for (let i = 0; i < this.myData.length; i++) {
+      if(this.myData[i].replies.length > 0){
+          console.log(this.myData[i].replies);
+          this.replyData = this.myData[i].replies
+    }
+  }
+}
+
+
+
 }
 </script>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="container-fluid" id="wrapper">
     <div id="comments-container" class="bg-info">
 
-      <div id="comments" class="bg-light rounded-2 mb-1">
+      <div v-for="comment in myData" :key="comment" id="comments" class="bg-light rounded-2 mb-1">
         <div id="counter-div" >
           <div id="count" class="rounded-3">
             <span id="ops"><b>+</b></span>
@@ -10,18 +10,14 @@
             <span id="ops"><b>-</b></span>
           </div>
 
-          <img @click="handleReply" v-if="showReply" src="../assets/icon-arrow-up.svg" alt="arrowup">
-          <img @click="handleReply" v-else src="../assets/icon-arrow-down.svg" alt="arrowdown">
-          
-
         </div>
         <div id="text-wrapper" class="pt-2">
 
           <div id="comment-info">
             <div id="first-info">
-              <img src="../assets/avatars/image-amyrobson.png" alt="amyrobson">
-              <h6 class="m-0"><b>amyrobson</b></h6>
-              <span>1 month ago</span>
+              <img src="./images/avatars/image-amyrobson.png" :alt="comment.user.username">
+              <h6 class="m-0"><b>{{comment.user.username}}</b></h6>
+              <span>{{comment.createdAt}}</span>
             </div>
 
             <div id="second-info">
@@ -32,49 +28,13 @@
           </div>
 
           <div id="comment-text" class="pt-2">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse quidem eum quis, minus, consectetur ab ad sit laudantium unde magnam consequuntur. Soluta nihil, provident maxime debitis quod exercitationem, rem et, reiciendis error suscipit facilis? Minima quidem quos qui vero labore optio maiores, expedita deleniti illo aliquid sunt id doloribus nostrum.</p>
+            <p>{{comment.content}}</p>
           </div>
 
         </div>
       </div>
 
-      <ReplyComponent :showReply = 'showReply' :handleReply = 'handleReply'/>
-
-      <div id="comments" class="bg-light rounded-2 mb-1">
-        <div id="counter-div" >
-          <div id="count" class="rounded-3">
-            <span id="ops"><b>+</b></span>
-            <span id="number"><b>12</b></span>
-            <span id="ops"><b>-</b></span>
-          </div>
-
-        </div>
-
-        <div id="text-wrapper" class="pt-2">
-
-          <div id="comment-info">
-            <div id="first-info">
-              <img src="../assets/avatars/image-amyrobson.png" alt="amyrobson">
-              <h6 class="m-0"><b>amyrobson</b></h6>
-              <span>1 month ago</span>
-            </div>
-
-            <div id="second-info">
-              <img src="../assets/icon-reply.svg" alt="reply">
-              <p class="m-0">Reply</p>
-            </div>
-            
-          </div>
-
-          <div id="comment-text" class="pt-2">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse quidem eum quis, minus, consectetur ab ad sit laudantium unde magnam consequuntur. Soluta nihil, provident maxime debitis quod exercitationem, rem et, reiciendis error suscipit facilis? Minima quidem quos qui vero labore optio maiores, expedita deleniti illo aliquid sunt id doloribus nostrum.</p>
-          </div>
-
-        </div>
-
-      </div>
-
-      <ReplyComponent :showReply = 'showReply' :handleReply = 'handleReply'/>
+      <ReplyComponent />
 
     </div>
 
@@ -83,23 +43,19 @@
 </template>
 
 <script>
-import ReplyComponent from '../components/ReplyComponent.vue'
+import commentData from "../data.json"
+import ReplyComponent from "../components/ReplyComponent.vue"
 export default {
+  components:{ReplyComponent},
   name: 'HomeView',
-  components: {ReplyComponent},
   data() {
     return {
-      showReply:false,
+      myData : commentData.comments,
+      userImage : []
     }
   },
-
-  methods: {
-    handleReply() {
-      this.showReply = !this.showReply
-    }
-
-  }
 }
+
 </script>
 
 <style>
